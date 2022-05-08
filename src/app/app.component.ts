ac2760099eb86d063e37c6e4bf35ea8ad315350e
Elchaninov-Fedor <fedor.elchaninov@tul.cz>
2022-05-08 23:01:33 +0200

    added scanner component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  scanResult: any='';
   title = 'QRScanner';
+  onCodeResult(result:string){
+    this.scanResult=result;
+  }
 }
